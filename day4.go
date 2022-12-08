8432f2b7e101780a94f9879116acf81a0cebc46e
rbaker <richardgbaker@yahoo.co.uk>
2022-12-08 00:32:54 +0000

    cleanup

--- day4.go
+++ day4.go
@@ -14,18 +14,11 @@ func day4part1() int {
 
 	for _, pairing := range pairings {
 
-		elves := strings.Split(pairing, ",")
-		elf1 := strings.Split(elves[0], "-")
-		elf2 := strings.Split(elves[1], "-")
-		elf1min, _ := strconv.Atoi(elf1[0])
-		elf1max, _ := strconv.Atoi(elf1[1])
-		elf2min, _ := strconv.Atoi(elf2[0])
-		elf2max, _ := strconv.Atoi(elf2[1])
+		elf1min, elf1max, elf2min, elf2max := getElves(pairing)
 
 		if (elf1min <= elf2min && elf1max >= elf2max) || (elf1min >= elf2min && elf1max <= elf2max) {
 			contained++
 		}
-
 	}
 
 	return contained
@@ -36,19 +29,23 @@ func day4part2() int {
 
 	for _, pairing := range pairings {
 
-		elves := strings.Split(pairing, ",")
-		elf1 := strings.Split(elves[0], "-")
-		elf2 := strings.Split(elves[1], "-")
-		elf1min, _ := strconv.Atoi(elf1[0])
-		elf1max, _ := strconv.Atoi(elf1[1])
-		elf2min, _ := strconv.Atoi(elf2[0])
-		elf2max, _ := strconv.Atoi(elf2[1])
+		elf1min, elf1max, elf2min, elf2max := getElves(pairing)
 
 		if (elf1min <= elf2min && elf1max >= elf2min) || (elf1min >= elf2min && elf1min <= elf2max) {
 			contained++
 		}
-
 	}
 
 	return contained
+}
+
+func getElves(pairing string) (int, int, int, int) {
+	elves := strings.Split(pairing, ",")
+	elf1 := strings.Split(elves[0], "-")
+	elf2 := strings.Split(elves[1], "-")
+	elf1min, _ := strconv.Atoi(elf1[0])
+	elf1max, _ := strconv.Atoi(elf1[1])
+	elf2min, _ := strconv.Atoi(elf2[0])
+	elf2max, _ := strconv.Atoi(elf2[1])
+	return elf1min, elf1max, elf2min, elf2max
 }
